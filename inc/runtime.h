5d863999ed36092951e88938420759e84d715109
PolZeck <paul.ledieu@gmail.com>
2025-11-06 13:29:12 +0100

    souris is ok but if a wild move on the mouse is did, the cursor can spawn outside the window

--- inc/runtime.h
+++ inc/runtime.h
@@ -3,142 +3,164 @@
 /*                                                        :::      ::::::::   */
 /*   runtime.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcosson <lcosson@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pol <pol@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 10:24:23 by pledieu           #+#    #+#             */
-/*   Updated: 2025/10/30 14:01:48 by lcosson          ###   ########.fr       */
+/*   Updated: 2025/11/06 13:25:21 by pol              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RUNTIME_H
-# define RUNTIME_H
+#define RUNTIME_H
 
 #include "cub3d.h"
 #include "mlx.h"
 #include "raycast.h"
-# include "vec2.h"
+#include "vec2.h"
 #include <math.h>
 
-# define MINIMAP_TILE 8
-# define MINIMAP_W    200
-# define MINIMAP_H    200
-# define MINIMAP_MARGIN_X 10
-# define MINIMAP_MARGIN_Y 10
+#define MINIMAP_TILE 8
+#define MINIMAP_W 200
+#define MINIMAP_H 200
+#define MINIMAP_MARGIN_X 10
+#define MINIMAP_MARGIN_Y 10
 
-/* Keycodes (Linux/X11) */
-#define KEY_ESC   65307
-#define KEY_W     119
-#define KEY_D     97
-#define KEY_S     115
-#define KEY_A     100
-#define KEY_LEFT  65361
+#define EVT_MOTION 6
+#define MSK_MOTION (1L << 6)
+
+#ifndef KEY_ALT_L
+#define KEY_ALT_L 65513
+#endif
+#ifndef KEY_ALT_R
+#define KEY_ALT_R 65514
+#endif
+
+#define KEY_ESC 65307
+#define KEY_W 119
+#define KEY_D 97
+#define KEY_S 115
+#define KEY_A 100
+#define KEY_LEFT 65361
 #define KEY_RIGHT 65363
 
-typedef struct s_minimap {
-	int enabled;    // 1 = affichée, 0 = off
-	int tile;       // pixels par tuile
-	int w;          // largeur de la zone minimap (en pixels)
-	int h;          // hauteur de la zone minimap (en pixels)
-	int margin_x;   // position à l'écran (offset X)
-	int margin_y;   // position à l'écran (offset Y)
+typedef struct s_minimap
+{
+	int enabled;
+	int tile;
+	int w;
+	int h;
+	int margin_x;
+	int margin_y;
 } t_minimap;
 
-typedef struct s_img {
-	void	*img;
-	char	*addr;
-	int		bpp;
-	int		line_len;
-	int		endian;
-	int		w;
-	int		h;
-}	t_img;
+typedef struct s_img
+{
+	void *img;
+	char *addr;
+	int bpp;
+	int line_len;
+	int endian;
+	int w;
+	int h;
+} t_img;
 
-typedef struct s_texset {
-	t_img	no;
-	t_img	so;
-	t_img	we;
-	t_img	ea;
-}	t_texset;
+typedef struct s_texset
+{
+	t_img no;
+	t_img so;
+	t_img we;
+	t_img ea;
+} t_texset;
 
-/* Player vectorisé */
-typedef struct s_player {
-	t_v2 pos;     /* à la place de pos_x/pos_y */
-	t_v2 dir;     /* à la place de dir_x/dir_y */
-	t_v2 plane;   /* à la place de plane_x/plane_y */
+typedef struct s_player
+{
+	t_v2 pos;
+	t_v2 dir;
+	t_v2 plane;
 } t_player;
 
-/* Ray vectorisé */
-typedef struct s_ray {
-	double	camera_x;
-	t_v2	ray_dir;   /* remplace ray_dir_x / ray_dir_y */
-	t_v2	delta;     /* remplace delta_x / delta_y     */
-	t_v2	sdist;      /* remplace side_x / side_y        */
-	int		map_x, map_y;
-	int		step_x, step_y;
-	int		hit;
-	int		side;
-	double  t;
-	double	perp_dist;
-	int		line_h;
-	int		draw_start, draw_end;
+typedef struct s_ray
+{
+	double camera_x;
+	t_v2 ray_dir;
+	t_v2 delta;
+	t_v2 sdist;
+	int map_x, map_y;
+	int step_x, step_y;
+	int hit;
+	int side;
+	double t;
+	double perp_dist;
+	int line_h;
+	int draw_start, draw_end;
 } t_ray;
 
+typedef struct s_app
+{
+	void *mlx;
+	void *win;
+	int win_w;
+	int win_h;
+	t_img frame;
+	int ceil_color;
+	int floor_color;
+	t_config cfg;
+	t_player pl;
+	int key_w, key_a, key_s, key_d;
+	int key_left, key_right;
+	t_texset tex;
+	t_minimap minimap;
+	int mouse_enabled;
+	int mouse_inited;
+	int mouse_last_x;
+	int mouse_last_y;
+	double mouse_sensi;
+	int mouse_warping;
+	int center_x;
+	int center_y;
+} t_app;
 
-typedef struct s_app {
-	void		*mlx;
-	void		*win;
-	int			win_w;
-	int			win_h;
-	t_img		frame;
-	int			ceil_color;
-	int			floor_color;
-	t_config	cfg;
-	t_player	pl;
-	int			key_w, key_a, key_s, key_d;
-	int			key_left, key_right;
-	t_texset	tex;
-	t_minimap  minimap;
-}	t_app;
-	
-typedef struct s_orient {
-	double	dx;
-	double	dy;
-	double	px;
-	double	py;
-}	t_orient;
+typedef struct s_orient
+{
+	double dx;
+	double dy;
+	double px;
+	double py;
+} t_orient;
 
-typedef enum e_face {
-	FACE_NO, FACE_SO, FACE_WE, FACE_EA
-}	t_face;
+typedef enum e_face
+{
+	FACE_NO,
+	FACE_SO,
+	FACE_WE,
+	FACE_EA
+} t_face;
 
-
-/* chargement / destruction */
-int  load_textures(t_app *a);
+int load_textures(t_app *a);
 void destroy_textures(t_app *a);
 
-/* sampling */
-int  texel_at(const t_img *tex, int u, int v);
+int texel_at(const t_img *tex, int u, int v);
 
-/* API runtime */
-int		run_app(t_config *cfg);
+int run_app(t_config *cfg);
 
-/* Hooks / player */
-int		on_close(t_app *a);
-int		on_key_down(int keycode, t_app *a);
-int		on_key_up(int keycode, t_app *a);
-int		on_loop(t_app *a);
-void	init_player_from_cfg(t_app *a);
-void	update_player(t_app *a, double move_speed, double rot_speed);
-int		rgb24(int r, int g, int b);
-void	put_px(t_img *img, int x, int y, int color);
-void	fill_half_screens(t_app *a);
+int on_close(t_app *a);
+int on_key_down(int keycode, t_app *a);
+int on_key_up(int keycode, t_app *a);
+int on_loop(t_app *a);
+void init_player_from_cfg(t_app *a);
+void update_player(t_app *a, double move_speed, double rot_speed);
+int rgb24(int r, int g, int b);
+void put_px(t_img *img, int x, int y, int color);
+void fill_half_screens(t_app *a);
 
-void	new_frame(t_app *a, int w, int h);
-void	init_mlx_and_window(t_app *a);
-void	setup_hooks(t_app *a);
-void	init_player_from_cfg(t_app *a);
-int		run_app(t_config *cfg);
+void new_frame(t_app *a, int w, int h);
+void init_mlx_and_window(t_app *a);
+void setup_hooks(t_app *a);
+void init_player_from_cfg(t_app *a);
+int run_app(t_config *cfg);
+int on_mouse_move(int x, int y, t_app *a);
+void mouse_enable(t_app *a, int enable);
 
-void	minimap_draw(t_app *a); /*pour la minimap*/
+void minimap_draw(t_app *a);
 
 #endif
--- src/app/init.c
+++ src/app/init.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcosson <lcosson@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pol <pol@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 10:26:21 by pledieu           #+#    #+#             */
-/*   Updated: 2025/10/30 13:07:38 by lcosson          ###   ########.fr       */
+/*   Updated: 2025/11/06 13:07:15 by pol              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "runtime.h"
 
-static void	setup_colors(t_app *a, t_config *cfg)
+static void setup_colors(t_app *a, t_config *cfg)
 {
 	a->ceil_color = rgb24(cfg->ceiling.r, cfg->ceiling.g, cfg->ceiling.b);
 	a->floor_color = rgb24(cfg->floor.r, cfg->floor.g, cfg->floor.b);
 }
 
-static void	apply_orient(t_app *a, t_orient o)
+static void apply_orient(t_app *a, t_orient o)
 {
 	a->pl.dir_x = o.dx;
 	a->pl.dir_y = o.dy;
@@ -26,9 +26,9 @@ static void	apply_orient(t_app *a, t_orient o)
 	a->pl.plane_y = o.py;
 }
 
-static void	set_dir_from_char(t_app *a, char d)
+static void set_dir_from_char(t_app *a, char d)
 {
-	t_orient	o;
+	t_orient o;
 
 	if (d == 'N')
 		o = (t_orient){0.0, -1.0, 0.66, 0.0};
@@ -41,25 +41,29 @@ static void	set_dir_from_char(t_app *a, char d)
 	apply_orient(a, o);
 }
 
-void	init_player_from_cfg(t_app *a)
+void init_player_from_cfg(t_app *a)
 {
 	a->pl.pos_x = a->cfg.player_x + 0.5;
 	a->pl.pos_y = a->cfg.player_y + 0.5;
 	set_dir_from_char(a, a->cfg.player_dir);
 }
 
-int	run_app(t_config *cfg)
+int run_app(t_config *cfg)
 {
-	t_app	a;
+	t_app a;
 
 	ft_bzero(&a, sizeof(a));
+	a.mouse_warping = 0;
+	a.mouse_sensi = 0.003;
+	a.center_x = a.win_w / 2;
+	a.center_y = a.win_h / 2;
 	a.cfg = *cfg;
 	a.win_w = 1024;
 	a.win_h = 768;
-	a.minimap.enabled  = 1;                 // active par défaut
-	a.minimap.tile     = MINIMAP_TILE;
-	a.minimap.w        = MINIMAP_W;
-	a.minimap.h        = MINIMAP_H;
+	a.minimap.enabled = 1;
+	a.minimap.tile = MINIMAP_TILE;
+	a.minimap.w = MINIMAP_W;
+	a.minimap.h = MINIMAP_H;
 	a.minimap.margin_x = MINIMAP_MARGIN_X;
 	a.minimap.margin_y = MINIMAP_MARGIN_Y;
 	setup_colors(&a, cfg);
